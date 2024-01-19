650c528fb78430d989295ed8cafe109863d92832
tomas_mokry <tomas.mokry@gmail.com>
2024-01-19 21:06:46 +0100

    bug fix

--- src/main/java/org/example/Kitten.java
+++ src/main/java/org/example/Kitten.java
@@ -3,6 +3,7 @@ package org.example;
 public class Kitten {
     public void bla(){
         //here is wery important bug fix
+        //here is second very important bug fix
         System.out.println("bla");
         System.out.println("change");
         System.out.println("This is a bug fix");
