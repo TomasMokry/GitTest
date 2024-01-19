5c4e7fcca4d86b36103d7e846401ad2dabbfa33c
tomas_mokry <tomas.mokry@gmail.com>
2024-01-19 20:24:14 +0100

    fox adding fields and constructor

--- src/main/java/org/example/Fox.java
+++ src/main/java/org/example/Fox.java
@@ -1,5 +1,11 @@
 package org.example;
 
 public class Fox {
+    private String name;
+    private int number;
 
+    public Fox(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
 }
