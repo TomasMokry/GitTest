c3ec6afbec7bf176b50243a3ba6c853b7c03291e
tomas_mokry <tomas.mokry@gmail.com>
2024-01-19 20:44:13 +0100

    comit for wolf

--- src/main/java/org/example/Wolf2.java
+++ src/main/java/org/example/Wolf2.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Wolf2 {
+    private String name;
+    private int number;
+    public Wolf2(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
 }
