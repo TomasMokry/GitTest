34c0eda4dd92d062ad5ce7eb9731b165e4a5f199
tomas_mokry <tomas.mokry@gmail.com>
2024-01-19 20:09:22 +0100

    I am commiting somthing here

--- src/main/java/org/example/Kitten.java
+++ src/main/java/org/example/Kitten.java
@@ -3,5 +3,6 @@ package org.example;
 public class Kitten {
     public void bla(){
         System.out.println("bla");
+        System.out.println("change");
     }
 }
---
+++ src/main/java/org/example/Wolf.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Wolf {
+    private String name;
+    private int age;
+
+    private static final int ID = 1;
+
+    public Wolf(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
